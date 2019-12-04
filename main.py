b11355957998312df92f9722c46ea8ad185b009a
Thevenel Joazard <thevenel25@gmail.com>
2019-12-04 09:46:44 -0500

    Created structure of the project

---
+++ main.py
@@ -0,0 +1,22 @@
+class Dominoes:
+    def __init__(self, players, bones):
+        self.players = 2
+        self.bones = 28
+    
+    def draw(self):
+        pass
+
+    def start(self):
+        pass
+
+    def end(self):
+        pass
+
+class Bones:
+    def __init__(self, phase_A, phase_B, double):
+        self.phase_A = random
+        self.phase_B = random
+        self.double = #equality A and B
+
+        self.bones = []
+        
